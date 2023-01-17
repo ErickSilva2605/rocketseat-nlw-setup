319d59ae1f1c5f7cdf8cb9098fb48afb0b7ddd83
ErickSilva2605 <erick.dev.it@gmail.com>
2023-01-16 23:16:13 -0300

    Instalando e configurando cors no back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,19 @@
-import Fastify from "fastify";
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get('/hello', () => {
-  return 'Hello NLW'
-});
+app.register(cors);
 
 app.listen({
   port: 3333,
 }).then(() => {
   console.log('Server running in port: 3333!')
+});
+
+app.get('/habits', async () => {
+  const habits = await prisma.habit.findMany();
+  return habits;
 });
