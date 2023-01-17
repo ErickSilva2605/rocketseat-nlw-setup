3c049e0c2cbb1397de0148d7cc64fad1dd610cb5
ErickSilva2605 <erick.dev.it@gmail.com>
2023-01-16 23:52:18 -0300

    Configurando tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,15 @@
+import './styles/global.css';
+
+import { Habit } from "./components/Habit"
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Habit completed={3} />
+      <Habit completed={10} />
+      <Habit completed={20} />
+      <Habit completed={30} />
+    </div>
   )
 }
 
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
   theme: {
     extend: {},
   },
