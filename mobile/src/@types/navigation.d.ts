0794865b8d8a951930af28a51b9638a3c8bacc7c
ErickSilva2605 <erick.dev.it@gmail.com>
2023-01-21 15:42:43 -0300

    Configurando a navegação para a tela de novo hábito

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined;
+      newHabit: undefined;
+      habit: {
+        date: string;
+      }
+    }
+  }
+}
