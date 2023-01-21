0a786f1b5a6e5867cebc8ebd013d2a6f457a2eac
ErickSilva2605 <erick.dev.it@gmail.com>
2023-01-21 11:19:59 -0300

    Criando o popover dos hábitos

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,23 @@
+import * as Popover from '@radix-ui/react-popover';
+import { ProgressBar } from './ProgressBar';
+
 export function HabitDay() {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">sábado</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">21/01</span>
+
+          <ProgressBar progress={75}/>
+
+          <Popover.Arrow
+            height={8}
+            width={16}
+            className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
