0620b935e98d110e77f356af216d28e7389c3703
ErickSilva2605 <erick.dev.it@gmail.com>
2023-01-21 15:59:04 -0300

    Criando botão de voltar

--- mobile/src/screens/NewHabit.tsx
+++ mobile/src/screens/NewHabit.tsx
@@ -1,9 +1,13 @@
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import { BackButton } from '../components/BackButton';
 
 export function NewHabit() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
 
+      </ScrollView>
     </View>
   );
 }
