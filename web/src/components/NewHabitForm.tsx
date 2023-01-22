993865e312683a9d94ae0ecd9cea8ab7ccf4f156
ErickSilva2605 <erick.dev.it@gmail.com>
2023-01-22 02:06:54 -0300

    Criando os checkboxes na web

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,4 +1,15 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado'
+]
 
 export function NewHabitForm() {
   return (
@@ -24,6 +35,32 @@ export function NewHabitForm() {
       >
         Qual a recorrência?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+        {
+          availableWeekDays.map((weekDay, index) => {
+            return (
+              <Checkbox.Root
+                key={weekDay}
+                className="flex items-center gap-3 group"
+              >
+                <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                  <Checkbox.Indicator>
+                    <Check
+                      size={20}
+                      className="text-white"
+                    />
+                  </Checkbox.Indicator>
+                </div>
+
+                <span className="text-white leading-tight">
+                  {weekDay}
+                </span>
+              </Checkbox.Root>
+            )
+          })
+        }
+      </div>
 
       <button
         type="submit"
