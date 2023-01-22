3488dc307ed8c3950e492a7bf619a65cfaaf97fc
ErickSilva2605 <erick.dev.it@gmail.com>
2023-01-22 00:40:18 -0300

    Ajuste

--- mobile/src/screens/NewHabit.tsx
+++ mobile/src/screens/NewHabit.tsx
@@ -44,7 +44,7 @@ export function NewHabit() {
         </Text>
 
         <TextInput
-          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-2 focus:border-green-600"
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus: focus:border-green-600"
           placeholder="Ex.:Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[500]}
         />
