f1045a764a91fbabc96d4cca691ba69c8bb100cd
ErickSilva2605 <erick.dev.it@gmail.com>
2023-01-22 02:44:06 -0300

    Preparando formulario para enviar ao back-end

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,5 +1,6 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
-import { Check } from "phosphor-react";
+import { Check, Target } from "phosphor-react";
+import { FormEvent, useState } from "react";
 
 const availableWeekDays = [
   'Domingo',
@@ -12,8 +13,30 @@ const availableWeekDays = [
 ]
 
 export function NewHabitForm() {
+  const [title, setTitle] = useState('');
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleToggleWeekDay(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay)
+
+      setWeekDays(weekDaysWithRemovedOne)
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay]
+
+      setWeekDays(weekDaysWithAddedOne)
+    }
+  }
+
+  function createNewHabit(event: FormEvent) {
+    event.preventDefault();
+
+    console.log(title);
+    console.log(weekDays);
+  }
+
   return (
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label
         htmlFor="title"
         className="font-semibold leading-tight"
@@ -25,7 +48,8 @@ export function NewHabitForm() {
         id="title"
         type="text"
         placeholder="ex.: Exercícios, dormir bem, etc..."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 border-2 border-zinc-800"
+        onChange={(event) => setTitle(event.target.value)}
         autoFocus
       />
 
@@ -43,6 +67,7 @@ export function NewHabitForm() {
               <Checkbox.Root
                 key={weekDay}
                 className="flex items-center gap-3 group"
+                onCheckedChange={() => handleToggleWeekDay(index)}
               >
                 <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
                   <Checkbox.Indicator>
