32f1da92ab01c2015f2a359e70b86b478494479d
ErickSilva2605 <erick.dev.it@gmail.com>
2023-01-23 23:45:57 -0300

    Realizando toggle dos hábitos

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -35,6 +35,25 @@ export function HabitsList({ date }: HabitsListProps) {
     .endOf('day')
     .isBefore(new Date());
 
+  async function handleToggleHabit(habitId: string) {
+    await api.patch(`habits/${habitId}/toggle`);
+
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId);
+
+    let completedHabits: string[] = [];
+
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId);
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId];
+    }
+
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits
+    });
+  }
+
   return (
     <div className="mt-6 flex flex-col gap-3">
       {
@@ -44,6 +63,7 @@ export function HabitsList({ date }: HabitsListProps) {
               key={habit.id}
               checked={habitsInfo.completedHabits.includes(habit.id)}
               disabled={isDateInPast}
+              onCheckedChange={() => { handleToggleHabit(habit.id) }}
               className="flex items-center gap-3 group"
             >
               <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
